e2408a411371d2b4bc6a89fcf619c0ad96cc9504
David Gabrielyan <gabrielyandavo144707@gmail.com>
2021-07-19 00:21:13 +0400

    Corrected navbar responsiveness for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,32 @@
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
-import React from 'react'
+import { FiMenu } from 'react-icons/fi'
 
 import * as styles from '../styles/navbar.module.css';
 
 export default function Navigation() {
+  const [open, setOpen] = useState(false);
   return (
-    <nav>
-      <Link className={styles.main} to='/about'>David Gabrielyan</Link>
-      <div className={styles.links}>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/skillset'>Skillset</Link>
-        <Link to='/projects'>Portfolio & projects</Link>
-      </div>
-    </nav>
+    <>
+      <FiMenu className={styles.navIcon} onClick={() => {
+        console.log('open fired');
+        setOpen(!open)
+      }}/>
+      <nav className={styles.mainNav}>
+        <Link className={styles.main} to='/about'>David Gabrielyan</Link>
+        <div className={styles.links}>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/skillset'>Skillset</Link>
+          <Link to='/projects'>Portfolio & projects</Link>
+        </div>
+      </nav>
+      {open ? <div className={styles.mobileNav}>
+          <Link className={styles.mobileLinks} to='/'>Home</Link>
+          <Link className={styles.mobileLinks} to='/about'>About</Link>
+          <Link className={styles.mobileLinks} to='/skillset'>Skillset</Link>
+          <Link className={styles.mobileLinks} to='/projects'>Portfolio & projects</Link>
+      </div> : null}
+    </>
   )
 }
