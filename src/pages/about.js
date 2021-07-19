38e7128775bacaff38e6620403e3347bfcc78e8d
David Gabrielyan <gabrielyandavo144707@gmail.com>
2021-07-20 00:19:25 +0400

    Code clean-up (about page for iphone XR)

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,19 +8,19 @@ export default function about() {
   return (
     <Layout>
       <div className={styles.wrapper}>
-        <h1>About Me and my career</h1>
-        <h3>Hi. I'm David, 23 years old software engineer experienced in backend and frontend developmentand devops.</h3>
-        <h3>I started my career back in 2018 when I came back from army. As I was studying</h3>
-        <h3>in Armenian State University of Economics (Yes, I was an economist/accountant),</h3>
-        <h3>I started to learn programming myself and during 6 difficult and sleepless months</h3>
-        <h3>I've learnt a lot abount programming, different languages (Python, C, Java),</h3>
-        <h3>different algorithms, graphs theory and trees. When I started my internship in</h3>
-        <h3>my first company I started learning about Web, Network, backend and frontend</h3>
-        <h3>and became a web developer who's favorite language today is JavaScript/TypeScript.</h3>
-        <h3>Also during my experience I've learnt different devops technologies such as</h3>
-        <h3>Docker, Jenkins, AWS and Bash scripting. Today I'm only working with Linux OS,</h3>
-        <h3>because for me it's the best choice for developers (I'm doing all the staff by terminal ;) ).</h3>
-        <h3>At the end I wanna say that I've never regretted being a programmer !</h3>
+        <h1 className={styles.mainTitle}>About Me and my career</h1>
+        <h3 className={styles.contentHs}>Hi. I'm David, 23 years old software engineer experienced in backend and frontend developmentand devops.</h3>
+        <h3 className={styles.contentHs}>I started my career back in 2018 when I came back from army. As I was studying</h3>
+        <h3 className={styles.contentHs}>in Armenian State University of Economics (Yes, I was an economist/accountant),</h3>
+        <h3 className={styles.contentHs}>I started to learn programming myself and during 6 difficult and sleepless months</h3>
+        <h3 className={styles.contentHs}>I've learnt a lot abount programming, different languages (Python, C, Java),</h3>
+        <h3 className={styles.contentHs}>different algorithms, graphs theory and trees. When I started my internship in</h3>
+        <h3 className={styles.contentHs}>my first company I started learning about Web, Network, backend and frontend</h3>
+        <h3 className={styles.contentHs}>and became a web developer who's favorite language today is JavaScript/TypeScript.</h3>
+        <h3 className={styles.contentHs}>Also during my experience I've learnt different devops technologies such as</h3>
+        <h3 className={styles.contentHs}>Docker, Jenkins, AWS and Bash scripting. Today I'm only working with Linux OS,</h3>
+        <h3 className={styles.contentHs}>because for me it's the best choice for developers (I'm doing all the staff by terminal ;) ).</h3>
+        <h3 className={styles.contentHs}>At the end I wanna say that I've never regretted being a programmer !</h3>
       </div>
       <h1 className={styles.experienceTitle}>Experience in companies</h1>
       <Container className={styles.container}>
